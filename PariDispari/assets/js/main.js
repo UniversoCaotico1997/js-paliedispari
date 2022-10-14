6e1314892e72d8275b76f315b820b6ff74d6d7a5
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-14 13:03:28 +0200

    revisone pari o dispari

--- PariDispari/assets/js/main.js
+++ PariDispari/assets/js/main.js
@@ -18,17 +18,17 @@
 // chiedimao all'utente discelgiere tra pari e dispari 
 
 const choice = prompt(`Scegli pari o dispari`);
-console.log(choice);
+// console.log(choice);
 
 // chiediamo all'utente di scegliere un numero da 1 a 5 
 
-const userNumber = Number(prompt(`Scegli da 1 a 5`))
-console.log(userNumber);
+const userNumber = Number(prompt(`Scegli da 1 a 5`));
+// console.log(userNumber);
 
 // variabile con i numeri random generati dal computer 
 const randomNumber = genereteRandomNumber(1, 5);
 
-// somma dei numeri 
+
 
 
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -57,11 +57,6 @@ if (pariOdispari(userNumber + randomNumber) == 0) {
 
 
 
-// // ciclo for 
-// for (let i = 0; i < 5; i++) {
-//     const randomNumber = genereteRandomNumber(1, 5);
-//     console.log(randomNumber);
-    
-// }
+
 
 
