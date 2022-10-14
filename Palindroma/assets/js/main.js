787e20bc8396e7e96a201f102685554c6fb00032
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-14 16:44:21 +0200

    End Palindroma

--- Palindroma/assets/js/main.js
+++ Palindroma/assets/js/main.js
@@ -20,25 +20,35 @@ console.log(word);
 // scomponiamo la parola in un Array  
 
 let wordBrokenDown = word.split(``);
-console.log(wordBrokenDown);
+// console.log(wordBrokenDown);
 
 // Invertiamo il nostro Array 
 
 let = wordBrokenDown.reverse();
-console.log(wordBrokenDown);
+// console.log(wordBrokenDown);
 
 // Ricomponimao la parola 
 
 const invertedWord = wordBrokenDown.join(``);
-console.log(invertedWord);
+// console.log(invertedWord);
 
 // Creiamo una funzione 
 
 function palindroma(check) {
     
+    if (word === invertedWord) {
+        return true   
     
-
-    
+    }   return false
 }
 
 
+
+if (palindroma(word === invertedWord)) {
+    
+    console.log(`La parola è palindroma`);
+        
+ } else {
+        console.log(`La parola non è palindroma`);
+        
+ }
